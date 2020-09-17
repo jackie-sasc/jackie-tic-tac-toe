7757e17813294c7b99e03fa189c97df38e288fa1
jackie-sasc <jackie-sasc@dataindependence.net>
2020-09-17 08:14:00 -0600

    2 files modified

--- index.ts
+++ index.ts
@@ -1,13 +1,17 @@
 var tdElements = document.getElementsByTagName('TD');
+var isO = false;
 
-
-for(var tdElement of
- tdElements) {
-  tdElement.
-  addEventListener
-  ('click', handleClick);
+for(var tdElement of tdElements) {
+  tdElement. addEventListener('click', handleClick);
 }
 
-function handleClick() {
-  alert('hello');
+function handleClick(event) {
+  var target = event.target;
+  if (isO) {
+    target.innerHTML = 'O';
+    isO = false;
+  } else {
+    target.innerHTML = 'X';
+    isO = true;
+  }
 }
